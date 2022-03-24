e100bde85d8c4eae243e5f6ef4723deb5f0db5b5
Fatima Camero <fcamero12@gmail.com>
2022-03-24 05:56:35 -0700

    update write concern for addUser

--- src/dao/usersDAO.js
+++ src/dao/usersDAO.js
@@ -61,7 +61,10 @@ export default class UsersDAO {
       // TODO Ticket: Durable Writes
       // Use a more durable Write Concern for this operation.
       // await users.insertOne({ someField: "someValue" })
-      await users.insertOne({ name: userInfo.name, email: userInfo.email,  password: userInfo.password })
+      await users.insertOne(
+        { name: userInfo.name, email: userInfo.email,  password: userInfo.password },
+        { w: "majority" }
+      )
       return { success: true }
     } catch (e) {
       if (String(e).startsWith("MongoError: E11000 duplicate key error")) {
